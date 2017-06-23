b138b04c1f6e87faaf446b51b61ac487331cae44
Victor Chee <victor.qhj@gmail.com>
2017-06-23 14:24:34 +0800

    Update capture.

--- Live/Live/AudioCapture.swift
+++ Live/Live/AudioCapture.swift
@@ -14,39 +14,45 @@ final class AudioCapture: NSObject {
     
     /// 由外部传入，因为要和Video Capture共享同一个sesstion
     var session: AVCaptureSession?
-    var captureOutput: AVCaptureAudioDataOutput!
-    var captureInput: AVCaptureDeviceInput!
+    fileprivate var captureOutput: AVCaptureAudioDataOutput?
+    fileprivate var captureInput: AVCaptureDeviceInput?
     
     fileprivate var outputHandler: OutputHandler?
     
+    // MARK: - Configurations
+    
     fileprivate func configureCaptureOutput() {
         guard let session = self.session else { return }
-        if captureOutput != nil {
+        if let captureOutput = self.captureOutput {
             captureOutput.setSampleBufferDelegate(nil, queue: nil)
             session.removeOutput(captureOutput)
         }
         
         captureOutput = AVCaptureAudioDataOutput()
-        captureOutput.setSampleBufferDelegate(self, queue: captureQueue)
+        captureOutput!.setSampleBufferDelegate(self, queue: captureQueue)
         if session.canAddOutput(captureOutput) {
             session.addOutput(captureOutput)
         }
     }
     
     fileprivate func configureCaptureInput() {
-        guard let device = AVCaptureDevice.defaultDevice(withMediaType: AVMediaTypeAudio) else { return }
         guard let session = self.session else { return }
+        if let captureInput = self.captureInput {
+            session.removeInput(captureInput)
+        }
         
         do {
+            let device = AVCaptureDevice.defaultDevice(withMediaType: AVMediaTypeAudio)
             captureInput = try AVCaptureDeviceInput(device: device)
-            session.automaticallyConfiguresApplicationAudioSession = true
             if session.canAddInput(captureInput) {
                 session.addInput(captureInput)
             }
         } catch {
-            print(error)
+            print("Audio Capture Input Error: \(error)")
         }
     }
+    
+    // MARK: - Methods
     
     func attachMicrophone() {
         configureCaptureOutput()
--- Live/Live/VideoCapture.swift
+++ Live/Live/VideoCapture.swift
@@ -16,30 +16,28 @@ final class VideoCapture: NSObject {
     
     /// 由外部传入，因为要和Audio Capture共享同一个sesstion
     var session: AVCaptureSession?
-    fileprivate var captureOutput: AVCaptureVideoDataOutput!
+    fileprivate var captureOutput: AVCaptureVideoDataOutput?
     fileprivate var captureInput: AVCaptureDeviceInput?
+    
     fileprivate var outputHandler: OutputHandler?
 
     var videoOrientation = AVCaptureVideoOrientation.portrait {
         didSet {
-            if videoOrientation == oldValue { return }
-            configureVideoOrientation()
+            if videoOrientation != oldValue {
+                configureVideoOrientation()
+            }
         }
     }
     
+    // MARK: - Configurations
+    
     fileprivate func configureVideoOrientation() {
-        if let connection = videoPreviewView.layer.value(forKey: "connection") as? AVCaptureConnection {
-            if connection.isVideoOrientationSupported {
-                connection.videoOrientation = videoOrientation
-            }
+        if let connection = videoPreviewView.layer.value(forKey: "connection") as? AVCaptureConnection, connection.isVideoOrientationSupported {
+            connection.videoOrientation = videoOrientation
         }
         
-        guard let output = captureOutput else { return }
-        
-        if let connection = output.connection(withMediaType: AVMediaTypeVideo) {
-            if connection.isVideoOrientationSupported {
-                connection.videoOrientation = videoOrientation
-            }
+        if let output = captureOutput, let connection = output.connection(withMediaType: AVMediaTypeVideo), connection.isVideoOrientationSupported {
+            connection.videoOrientation = videoOrientation
         }
     }
     
@@ -49,19 +47,19 @@ final class VideoCapture: NSObject {
     
     fileprivate func configureCaptureOutput() {
         guard let session = self.session else { return }
-        if captureOutput != nil {
+        if let captureOutput = self.captureOutput {
             session.removeOutput(captureOutput)
         }
         
         captureOutput = AVCaptureVideoDataOutput()
-        captureOutput.alwaysDiscardsLateVideoFrames = true
-        captureOutput.videoSettings = [(kCVPixelBufferPixelFormatTypeKey as String): NSNumber(value: kCVPixelFormatType_420YpCbCr8BiPlanarFullRange)]
-        captureOutput.setSampleBufferDelegate(self, queue: captureQueue)
+        captureOutput!.alwaysDiscardsLateVideoFrames = true
+        captureOutput!.videoSettings = [(kCVPixelBufferPixelFormatTypeKey as String): NSNumber(value: kCVPixelFormatType_420YpCbCr8BiPlanarFullRange)]
+        captureOutput!.setSampleBufferDelegate(self, queue: captureQueue)
         if session.canAddOutput(captureOutput) {
             session.addOutput(captureOutput)
         }
         
-        for connection in captureOutput.connections {
+        for connection in captureOutput!.connections {
             guard let connection = connection as? AVCaptureConnection else { continue }
             if connection.isVideoOrientationSupported {
                 connection.videoOrientation = videoOrientation
@@ -71,19 +69,18 @@ final class VideoCapture: NSObject {
     
     fileprivate func configureCaptureInput() {
         guard let session = self.session else { return }
+        if let captureInput = self.captureInput {
+            session.removeInput(captureInput)
+        }
         
         do {
-            if captureInput != nil {
-                session.removeInput(captureInput)
-            }
-            
             let device = AVCaptureDevice.defaultDevice(withMediaType: AVMediaTypeVideo)
             captureInput = try AVCaptureDeviceInput(device: device)
             if session.canAddInput(captureInput) {
                 session.addInput(captureInput)
             }
         } catch {
-            print(error)
+            print("Video Capture Input Error: \(error)")
         }
     }
     
@@ -95,6 +92,8 @@ final class VideoCapture: NSObject {
         }
         session.commitConfiguration()
     }
+    
+    // MARK: - Methods
     
     func attachCamera() {
         configureCaptureOutput()
